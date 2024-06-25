26e86d8e6854e90aa63965aea7138a79631b38ef
Sefa Pamuk <sefa76q@hotmail.de>
2024-06-25 12:06:36 +0200

    started survey window

--- SSLimPy/LIMsurvey/Covariance.py
+++ SSLimPy/LIMsurvey/Covariance.py
@@ -1,4 +1,19 @@
+from astropy import units as u
+from astropy import constants as c
+
+from time import time
+
+import numpy as np
+from SSLimPy.interface import config as cfg
+from copy import copy
+
 class Covariance:
     def __init__(self, cosmology, powerspectrum):
         self.cosmology = cosmology
-        self.powerspectrum = powerspectrum        
+        self.powerspectrum = powerspectrum       
+
+
+    def L_side(self, zc, deltaz):
+        zgrid = np.array([zc - 0.5 * deltaz, zc + 0.5 * deltaz])
+        Lgrid = self.cosmology.comoving(zgrid)
+        return Lgrid[1] - Lgrid[0]
