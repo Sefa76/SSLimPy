fa38526d93cf9a4a56feed16169bb867302bfb53
Sefa Pamuk <sefa76q@hotmail.de>
2024-08-30 15:02:54 +0200

    find the typ0

--- SSLimPy/LIMsurvey/higherorder.py
+++ SSLimPy/LIMsurvey/higherorder.py
@@ -114,5 +114,26 @@ def TrispectrumL0(k1, mu1, ph1,
     vP = _linear_interpolate(kgrid, Pgrid, vk)
 
     # Compute over all ṕermutations of F2 F2 diagrams
+    T1 = vP[0] * vP[3] * vP[4] * vF2(k12, mu12, k1, -mu1, ph12 - ph1 - np.pi) * vF2(k34, mu34, k4, -mu4, ph34 - ph4 - np.pi)
+    T1 += vP[0] * vP[1] * vP[5] * vF2(k13, mu13, k1, -mu1, ph13 - ph1 - np.pi) * vF2(k24, mu24, k2, -mu2, ph24 - ph2 - np.pi)
+    T1 += vP[0] * vP[2] * vP[6] * vF2(k14, mu14, k1, -mu1, ph14 - ph1 - np.pi) * vF2(k23, mu23, k3, -mu3, ph23 - ph3 - np.pi)
+    T1 += vP[1] * vP[0] * vP[7] * vF2(k23, mu23, k2, -mu2, ph23 - ph2 - np.pi) * vF2(k14, mu14, k1, -mu1, ph14 - ph1 - np.pi)
+    T1 += vP[1] * vP[2] * vP[8] * vF2(k24, mu24, k2, -mu2, ph24 - ph2 - np.pi) * vF2(k13, mu13, k3, -mu3, ph13 - ph3 - np.pi)
+    T1 += vP[1] * vP[3] * vP[4] * vF2(k12, mu12, k2, -mu2, ph12 - ph2 - np.pi) * vF2(k34, mu34, k4, -mu4, ph34 - ph4 - np.pi)
+    T1 += vP[2] * vP[1] * vP[9] * vF2(k34, mu34, k3, -mu3, ph34 - ph3 - np.pi) * vF2(k12, mu12, k2, -mu2, ph12 - ph2 - np.pi)
+    T1 += vP[2] * vP[3] * vP[5] * vF2(k13, mu13, k3, -mu3, ph13 - ph3 - np.pi) * vF2(k24, mu24, k4, -mu4, ph24 - ph4 - np.pi)
+    T1 += vP[2] * vP[0] * vP[7] * vF2(k23, mu23, k3, -mu3, ph23 - ph3 - np.pi) * vF2(k14, mu14, k1, -mu1, ph14 - ph1 - np.pi)
+    T1 += vP[3] * vP[2] * vP[6] * vF2(k14, mu14, k4, -mu4, ph14 - ph4 - np.pi) * vF2(k23, mu23, k3, -mu3, ph23 - ph3 - np.pi)
+    T1 += vP[3] * vP[0] * vP[8] * vF2(k24, mu24, k4, -mu4, ph24 - ph4 - np.pi) * vF2(k13, mu13, k1, -mu1, ph13 - ph1 - np.pi)
+    T1 += vP[3] * vP[1] * vP[9] * vF2(k34, mu34, k4, -mu4, ph34 - ph4 - np.pi) * vF2(k12, mu12, k2, -mu2, ph12 - ph2 - np.pi)
+    T1 *= 4
+    # That should be all of them ...
 
     # Compute over all permutations of F3 diagrams
+    T2 = vP[0] * vP[1] * vP[2] * vF3(k1, mu1, ph1, k2, mu2, ph2, k3, mu3, ph3)
+    T2 += vP[1] * vP[2] * vP[3] * vF3(k2, mu2, ph2, k3, mu3, ph3, k4, mu4, ph4)
+    T2 += vP[2] * vP[3] * vP[0] * vF3(k3, mu3, ph3, k4, mu4, ph4, k1, mu1, ph1)
+    T2 += vP[3] * vP[0] * vP[1] * vF3(k4, mu4, ph4, k1, mu1, ph1, k2, mu2, ph2)
+    T2 *= 6
+
+    return T1 + T2
