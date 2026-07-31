c24d84c5ff669816aaba30efa3561d00538a76c0
jl-bernal <jlbernal@ifca.unican.es>
2026-08-01 00:58:56 +0200

    clearer effect of non preserving scatter in Lavbg, same effect

--- SSLimPy/cosmology/astro.py
+++ SSLimPy/cosmology/astro.py
@@ -241,7 +241,7 @@ class AstroFunctions:
             if self.astroparams["meanperserve_scatter"]:
                 Lpbar *= np.exp(0.5 * p * (p - 1) * (self.sigma_scatter * log10) ** 2)
             else:
-                Lpbar *= np.exp(0.5 * p * (self.sigma_scatter * log10) ** 2)
+                Lpbar *= np.exp(0.5 * p * p * (self.sigma_scatter * log10) ** 2)
 
             if "TonyLi" == self.model_name:
 
